c7c0c8f00c98c70770f22788a7ac473163957fbc
niloarauj <nilo@safei.com.br>
2025-01-28 11:52:28 -0300

    Implementação Bootstrap

--- MauroTec/src/App.tsx
+++ MauroTec/src/App.tsx
@@ -1,35 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { bg } from 'date-fns/locale';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <body>
+        <nav className="navbar fixed-top bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Fixed top</a>
+    </div>
+  </nav>
+  
+    </body>
+    
+  );
 }
 
-export default App
+export default App;
+
